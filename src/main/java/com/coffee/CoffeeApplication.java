e737068c0102f9c64dc63449df575339abf1b4ce
bluesky <bluesky@naver.com>
2025-10-03 10:29:01 +0900

    ddd

--- src/main/java/com/coffee/CoffeeApplication.java
+++ src/main/java/com/coffee/CoffeeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoffeeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeApplication.class, args);
 	}
-
 }
